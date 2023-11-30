e6211a46c57f94bd1ab70d8fe496e5dcad383aed
Mihir Jain <63667062+mixh@users.noreply.github.com>
2023-11-30 16:18:15 -0500

    add experience and settings page

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,21 @@
-/* eslint-disable no-unused-vars */
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import HomePage from "./Pages/HomePage";
+import SecondPage from "./Pages/Settings";
+import Navbar from "./Components/Navbar";
 import "./index.css";
 
 function App() {
   return (
-    <div className="">
-      <HomePage />
-    </div>
+    <Router>
+      <div className="App">
+        <Navbar />
+
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/second-page" element={<SecondPage />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
