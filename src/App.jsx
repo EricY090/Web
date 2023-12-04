88b5433324b63f9bf78f69403e00c0f083e0a1b0
EricY090 <116921922+EricY090@users.noreply.github.com>
2023-12-03 20:52:07 -0500

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -10,11 +10,27 @@ import { useEffect, useState } from "react";
 
 var dark = 0
 function App() {
+  const [color, setColor] = useState("white")
+  const click = () => {
+    if (dark==0){
+      setColor("black");
+      dark = 1
+    }
+    else {
+      setColor('white');
+      dark = 0
+    }
+  }
+  useEffect(()=>{
+    document.body.style.backgroundColor = color
+  }, [color])
   return (
     <Router>
       <div className="App">
         <Navbar />
-
+        <button onClick = {
+          () => {click()}
+        } className="text-lg font-semibold text-purple-600 bg-blue-100">Change the Mode</button>
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/second-page" element={<SecondPage />} />
