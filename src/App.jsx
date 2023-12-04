1958a11222342b732a0b602e1f93f1b9646a58f7
EricY090 <116921922+EricY090@users.noreply.github.com>
2023-12-03 20:42:46 -0500

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
-/* eslint-disable no-unused-vars */
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import HomePage from "./Pages/HomePage";
+import SecondPage from "./Pages/SettingsPage";
+import LeaderboardPage from "./Pages/LeaderboardPage";
+import Navbar from "./Components/Navbar";
 import "./index.css";
 import { useEffect, useState } from "react";
 
@@ -8,28 +10,35 @@ import { useEffect, useState } from "react";
 
 var dark = 0
 function App() {
-    const [color, setColor] = useState("white")
-    const click = () => {
-      if (dark==0){
-        setColor("black");
-        dark = 1
-      }
-      else {
-        setColor('white');
-        dark = 0
-      }
+  const [color, setColor] = useState("white")
+  const click = () => {
+    if (dark==0){
+      setColor("black");
+      dark = 1
     }
-    useEffect(()=>{
-      document.body.style.backgroundColor = color
-    }, [color])
-    return (
-      <div className="">
+    else {
+      setColor('white');
+      dark = 0
+    }
+  }
+  useEffect(()=>{
+    document.body.style.backgroundColor = color
+  }, [color])
+  return (
+    <Router>
+      <div className="App">
+        <Navbar />
         <button onClick = {
           () => {click()}
         } className="text-lg font-semibold text-purple-600 bg-blue-100">Change the Mode</button>
-        <HomePage/>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/second-page" element={<SecondPage />} />
+          <Route path="/leaderboard-page" element={<LeaderboardPage />} />
+        </Routes>
       </div>
-    )
+    </Router>
+  );
 }
 
 export default App;
