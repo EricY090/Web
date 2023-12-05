fd217eac1bb314ae82909458d7d724f58ea798db
Eric Yang <116921922+EricY090@users.noreply.github.com>
2023-12-05 00:35:10 -0500

    Add files via upload

---
+++ src/Pages/RatingPage.jsx
@@ -0,0 +1,11 @@
+import Experience from "../Components/Experience";
+
+const SecondPage = () => {
+  return (
+    <div className="">
+      <Experience />
+    </div>
+  );
+};
+
+export default SecondPage;
